539f11a6c72b8ef4cdf8f3ad2ed80baca9664f61
plexy <khansolo23@gmail.com>
2026-07-10 17:33:32 +0100

    Add font imports for Space Grotesk and JetBrains Mono
    JetBrain Mono Display Scaffold

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '@fontsource/space-grotesk/400.css'
+import '@fontsource/space-grotesk/500.css'
+import '@fontsource/space-grotesk/700.css'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/600.css'
+import '@fontsource/jetbrains-mono/700.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
